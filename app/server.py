c6500a9f53aad685d53ece9d0656ffd99fa5bf51
DESKTOP-RYZEN17\paul <paul@sonicviz.com>
2019-03-14 15:09:05 +0900

    Revert "Update tfms to ds_tfms ref: https://forums.fast.ai/t/transform-got-multiple-values-for-argument-tfms-lesson-2/37975/2"
    This reverts commit 558f8796876ae6b9faa3ca6fb7ab971319e88681.

--- app/server.py
+++ app/server.py
@@ -28,8 +28,7 @@ async def download_file(url, dest):
 async def setup_learner():
     await download_file(model_file_url, path/'models'/f'{model_file_name}.pth')
     data_bunch = ImageDataBunch.single_from_classes(path, classes,
-        # rename tfms to ds_tfms ref: https://forums.fast.ai/t/transform-got-multiple-values-for-argument-tfms-lesson-2/37975/2
-        ds_tfms=get_transforms(), size=224).normalize(imagenet_stats)
+        tfms=get_transforms(), size=224).normalize(imagenet_stats)
     learn = cnn_learner(data_bunch, models.resnet34, pretrained=False)
     learn.load(model_file_name)
     return learn
